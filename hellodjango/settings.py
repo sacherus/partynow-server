4252473a6468e19e0fc52cd36698db85607f13eb
Piotr Kowenzowski <sacherus@gmail.com>
2013-12-28 12:48:41 +0100

    Delete database

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
     }
 }
 
@@ -89,7 +89,7 @@ STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default='sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3'))
+DATABASES['default'] =  dj_database_url.config(default='sqlite:///'+os.path.join(BASE_DIR, 'sqlite3.db'))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
